7d32675a59146ec9daa64ac2c179a5477e542d5a
Gerry <madeiramateus@gmai.com>
2025-12-03 12:16:42 +0000

    Alteraçoes no nome das tabelas.

---
+++ 0_igreja_DB.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS igreja_system;
+
+CREATE DATABASE igreja_system;
+
+USE igreja_system;
--- 2_pap_oração.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Oracoes (
-   id INT(11) UNSIGNED NOT NULL  AUTO_INCREMENT PRIMARY KEY,
-    Data_pedido:DATETIME,
-    Data_Resposta DATETIME,
-    PRIMARY KEY(id)
-    mensagem:VARCHAR(100)
-    
-)
