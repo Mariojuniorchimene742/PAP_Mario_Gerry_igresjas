e17b038533a1568a89fc25b6fe6d5c95a959471b
Gerry <madeiramateus@gmai.com>
2025-12-08 14:20:41 +0000

    coloquei atributos na tabela 1 pastores.

---
+++ 1_igreja+_pastores.sql
@@ -0,0 +1,10 @@
+USE igreja+_system;
+
+DROP TABLE IF EXISTS pastor;
+CREATE TABLE pastor(
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(20),
+    email VARCHAR(100)
+);
+
